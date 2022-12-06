1c1564bc282f9f22be3129680bcbed12db4d5e1a
freakGE <esebua154@gmail.com>
2022-12-06 19:11:44 +0400

    Topic

---
+++ components/Topic.js
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+import Highlight from "./Highlight";
+import useWindowDimensions from "../components/WindowDimensions";
+
+const Topic = ({ topic }) => {
+  const { height, width } = useWindowDimensions();
+
+  const mediumWidth = 768;
+
+  return (
+    <motion.div className="overflow-hidden title-container">
+      <h1 className="relative title">
+        <Highlight text={topic} hide={true} title={true} />
+        <div className="absolute bottom-2.5 z-[-1] w-full border border-dark-primary" />
+      </h1>
+      <motion.div
+        initial={width >= mediumWidth ? { x: -750 } : { scaleX: 0 }}
+        animate={width >= mediumWidth ? { x: 0 } : { scaleX: 1 }}
+        transition={{ duration: 3 }}
+        className="title-line"
+      ></motion.div>
+    </motion.div>
+  );
+};
+
+export default Topic;
