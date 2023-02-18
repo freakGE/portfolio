c7628062fc060747fc6df0123887c5660223b157
freakGE <esebua154@gmail.com>
2023-02-18 21:35:37 +0400

    New Project: 'Watching Assistant'

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,10 @@
 import Project from "./Project";
 import Topic from "./Topic";
 
+import useWindowDimensions from "./WindowDimensions";
+
 const Projects = () => {
+  const { width } = useWindowDimensions();
   return (
     <section className="wrapper-container min-h-[20rem]">
       <div className="wrapper topic projects-container">
@@ -9,11 +12,26 @@ const Projects = () => {
         <div className="flex flex-col gap-y-24 mt-7 2tiny:gap-y-36 sm:gap-y-8 lg:gap-y-16">
           <Project
             toRight={true}
+            name="Watching Assistant"
+            smallExtra={true}
+            image={`thumbnail-watching-assistant.png`}
+            imageAlt={"Watching Assistant thumbnail"}
+            description={
+              width > 420
+                ? "Watching Assistant is a website for tracking movies and TV shows. Users can create watchlists, mark titles as watched or currently watching, and track their progress through TV shows by season and episode."
+                : "Watching Assistant is a website for tracking movies and TV shows. Users can create watchlists and track their progress through TV shows by season and episode."
+            }
+            usedSkills={["Next.js", "TypeScript", "MongoDB", "TailwindCSS"]}
+            code="https://github.com/freakGE/watching-assistant"
+            demo="https://watching-assistant.vercel.app/"
+          />
+          <Project
+            toRight={false}
             name="Michelangelo Exhibition"
             smallExtra={true}
             image={`thumbnail-michelangelo.png`}
             imageAlt={"Michelangelo exhibition thumbnail"}
-            description=" The idea was to create a website dedicated to the Michelangelo exhibition. The project includes his biography and art collection."
+            description="The idea was to create a website dedicated to the Michelangelo exhibition. The project includes his biography and art collection."
             usedSkills={[
               "Next.js",
               "TypeScript",
@@ -25,7 +43,7 @@ const Projects = () => {
             demo="https://michelangelo-exibition.vercel.app/"
           />
           <Project
-            toRight={false}
+            toRight={true}
             name="BookWorm"
             image={`thumbnail-bookworm.png`}
             imageAlt={"bookWorm thumbnail"}
@@ -40,7 +58,7 @@ const Projects = () => {
             demo="https://superb-genie-9f4542.netlify.app/"
           />
           <Project
-            toRight={true}
+            toRight={false}
             name="CLI Launcher"
             image={`thumbnail-dayz-cli-launcher.png`}
             imageAlt={"DayZ CLI launcher thumbnail"}
